b683850c73de75793494dd59c928a653c484439a
tom reilly <treilly94@live.com>
2020-04-23 20:15:25 +0100

    updated to sample with table output

--- function/exercise-tracker/__init__.py
+++ function/exercise-tracker/__init__.py
@@ -1,24 +1,19 @@
 import logging
+import uuid
+import json
 
 import azure.functions as func
 
+def main(req: func.HttpRequest, message: func.Out[str]) -> func.HttpResponse:
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    rowKey = str(uuid.uuid4())
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    data = {
+        "Name": "Output binding message",
+        "PartitionKey": "message",
+        "RowKey": rowKey
+    }
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    message.set(json.dumps(data))
+
+    return func.HttpResponse(f"Message created with the rowKey: {rowKey}")
